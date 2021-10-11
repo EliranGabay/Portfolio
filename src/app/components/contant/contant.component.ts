fec3485b8d4f869eea0e3247cca50a7c56ebb975
eliran gabay <34740857+EliranGabay@users.noreply.github.com>
2021-10-11 16:36:41 +0300

    contact page update

--- src/app/components/contant/contant.component.ts
+++ src/app/components/contant/contant.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{faShare}from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-contant',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contant.component.css']
 })
 export class ContantComponent implements OnInit {
-
+  faShare=faShare;
   constructor() { }
 
   ngOnInit(): void {
