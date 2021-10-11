287191ac3d215d650f8f8cc378ef3819cbb5794f
eliran gabay <34740857+EliranGabay@users.noreply.github.com>
2021-10-11 13:55:01 +0300

    fix navbar and sub main

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -9,9 +9,17 @@ import{faGithub,faLinkedin} from '@fortawesome/free-brands-svg-icons'
 export class NavbarComponent implements OnInit {
   faLinkedin=faLinkedin;
   faGithub=faGithub;
+  navbarVisible = false;
+  navbarOpen=true;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  collapseNav(){
+    this.navbarVisible = !this.navbarVisible;
+  }
+  navOpen(){
+    this.navbarOpen = !this.navbarOpen;
+  }
 }
